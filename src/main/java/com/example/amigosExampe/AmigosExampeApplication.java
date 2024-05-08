c3240d0ebf6802955d4a0f9f0f4baedb6a1d3a0e
mohamed tantawy <mtantawy2025@gmail.com>
2024-05-08 20:23:30 +0300

    init project

---
+++ src/main/java/com/example/amigosExampe/AmigosExampeApplication.java
@@ -0,0 +1,13 @@
+package com.example.amigosExampe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosExampeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosExampeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amigosExampe/AmigosExampeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amigosExampe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosExampeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
